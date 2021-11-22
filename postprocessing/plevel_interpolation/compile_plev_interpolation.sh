991e398bb548a979d4cceb0a4bbb53e19fff1d56
Regan Mudhar <rm811@emps-gv3.ex.ac.uk>
2021-11-22 14:51:53 +0000

    adjustments to .sh file to attempt to remedy compile errors based on SIT/ERB commits

--- postprocessing/plevel_interpolation/compile_plev_interpolation.sh
+++ postprocessing/plevel_interpolation/compile_plev_interpolation.sh
@@ -5,6 +5,14 @@ cd ./exec
 
 source $GFDL_BASE/src/extra/env/$GFDL_ENV
 
-../bin/mkmf -p plev.x -t ../bin/mkmf.template.ia64 -c "-Duse_netCDF" -a ../src ../src/path_names ../src/shared/mpp/include ../src/shared/include
+#compiler=${GFDL_MKMF_TEMPLATE:-ia64} #from stephen's change to .sh file
+
+#template=mkmf.template.${compiler} #from stephen's change to .sh file  
+
+# ../bin/mkmf -p plev.x -t $GFDL_BASE/src/extra/python/isca/templates/$template -c "-Duse_netCDF" -a ../src ../src/path_names ../src/shared/mpp/include ../src/shared/include #from stephen's change to .sh file  
+
+# ../bin/mkmf -p plev.x -t ../bin/mkmf.template.ia64 -c "-Duse_netCDF" -a ../src ../src/path_names ../src/shared/mpp/include ../src/shared/include #from old version of .sh file
+
+../bin/mkmf -p plev.x -t $GFDL_BASE/src/extra/python/isca/templates/mkmf.template.ia64 -c "-Duse_netCDF" -a ../src ../src/path_names ../src/shared/mpp/include ../src/shared/include #from emily's change to .sh file
 
 make -f Makefile
