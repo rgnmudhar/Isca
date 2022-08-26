71782cc91ff830d8a7f6fc9eef6caecefcb4462a
Regan Mudhar <rm811@emps-gv5.ex.ac.uk>
2022-08-26 12:02:37 +0100

    signposted options for including topography

--- exp/test_cases/polvani_kushner/polvani_kushner_test_case.py
+++ exp/test_cases/polvani_kushner/polvani_kushner_test_case.py
@@ -24,16 +24,16 @@ cb.compile()  # compile the source code to working directory $GFDL_WORK/codebase
 # and output diagnostics
 
 # if using local heating from file:
-#inputpath = 'input/asymmetry/' # INCLUDE THIS FOR HEATING
-#inputfile = 'w15a6p400f800g50_q6m2y45l800u200' # INCLUDE THIS FOR HEATING
+inputpath1 = 'input/polar_heating/' # INCLUDE THIS FOR HEATING
+inputfile1 = 'w15a4p800f800g50' # INCLUDE THIS FOR HEATING
+#inputpath2 = 'input/asymmetry/' # INCLUDE THIS FOR TOPOGRAPHY
+#inputfile2 = 'h4000m2l25u65' # INCLUDE THIS FOR TOPOGRAPHY
 
-#exp_name = 'PK_e0v4z13_'+inputfile # updated experiment name
-exp_name = 'PK_e0v1z18'
+exp_name = 'PK_e0v4z13_'+inputfile1 # updated experiment name
 exp = Experiment(exp_name, codebase=cb)
 
-#exp.inputfiles = [os.path.join(GFDL_BASE,'input/land_masks/era_land_t42.nc')]
-#exp.inputfiles = [os.path.join(GFDL_BASE,inputpath+inputfile+'.nc')] # INCLUDE THIS FOR HEATING
-
+exp.inputfiles = [os.path.join(GFDL_BASE,inputpath1+inputfile1+'.nc')] # INCLUDE THIS FOR HEATING
+#exp.inputfiles = [os.path.join(GFDL_BASE,inputpath2+inputfile2+'.nc')] # INCLUDE THIS FOR TOPOGRAPHY
 
 #Tell model how to write diagnostics
 diag = DiagTable()
@@ -44,17 +44,18 @@ diag.add_file('atmos_daily', 1, 'days', time_units='days') # added output of dai
 diag.add_field('dynamics', 'ps', time_avg=True)
 diag.add_field('dynamics', 'sphum', time_avg=True) # added diagnostic field for plevel_interp
 diag.add_field('dynamics', 'zsurf') # added diagnostic field for plevel_interp - zsurf is static so can't get time average
-diag.add_field('dynamics', 'bk')
-diag.add_field('dynamics', 'pk')
+#diag.add_field('dynamics', 'bk')
+#diag.add_field('dynamics', 'pk')
 diag.add_field('dynamics', 'ucomp', time_avg=True)
 diag.add_field('dynamics', 'vcomp', time_avg=True)
+diag.add_field('dynamics', 'omega', time_avg=True) # added diagnostic field for w
 diag.add_field('dynamics', 'temp', time_avg=True)
-diag.add_field('dynamics', 'vor', time_avg=True)
-diag.add_field('dynamics', 'div', time_avg=True)
+#diag.add_field('dynamics', 'vor', time_avg=True)
+#diag.add_field('dynamics', 'div', time_avg=True)
 diag.add_field('dynamics', 'height', time_avg=True) # added diagnostic field for height
 
 diag.add_field('hs_forcing', 'teq', time_avg=True) # added diagnostic field as sanity check on Teq
-#diag.add_field('hs_forcing', 'local_heating', time_avg=True) # INCLUDE THIS FOR HEATING
+diag.add_field('hs_forcing', 'local_heating', time_avg=True) # INCLUDE THIS FOR HEATING
 
 exp.diag_table = diag
 
@@ -85,8 +86,8 @@ namelist = Namelist({
         'surf_res': 0.5
     },
 
-    #'spectral_init_cond_nml': { # namelist required for topography added - INCLUDE THIS FOR TOPO
-    #    'topog_file_name': inputfile+'.nc', #input file name
+    #'spectral_init_cond_nml': { # namelist required for topography added - INCLUDE THIS FOR TOPOGRAPHY
+    #    'topog_file_name': inputfile2+'.nc', #input file name
     #    'topography_option': 'input' # take topography from input file
     #},
 
@@ -98,7 +99,7 @@ namelist = Namelist({
         'delv': 10.,       # lapse rate (default 10K)
         'eps': 0.,         # stratospheric latitudinal variation (default 0K) - NOTE: ±10 as per P-K paper
         'sigma_b': 0.7,    # boundary layer friction height (default p/ps = sigma = 0.7)
-        'vtx_gamma': 1.0, # experiment with different values of gamma
+        'vtx_gamma': 4.0, # experiment with different values of gamma
         'equilibrium_t_option': 'Polvani_Kushner', # add new option for polvani_kushner relaxation
         'strat_vtx': True, # default is True - set to False so that w_vtx=0 for no polar vortex
 
@@ -107,13 +108,13 @@ namelist = Namelist({
         'ks':    -4.,      # Boundary layer dependent cooling timescale (default 4 days)
         'kf':   -1.,       # BL momentum frictional timescale (default 1 days)
 
-        'z_ozone': 18.,     # added height of stratospheric heating source
+        'z_ozone': 13.,     # added height of stratospheric heating source
         'do_conserve_energy':   True,  # convert dissipated momentum into heat (default True)
-        'sponge_flag': True #,      # added sponge layer for simple damping in upper levels
+        'sponge_flag': True ,      # added sponge layer for simple damping in upper levels
 
         # variables for including heating
-        #'local_heating_option': 'from_file', # INCLUDE THIS FOR HEATING
-        #'local_heating_file': inputfile # INCLUDE THIS FOR HEATING
+        'local_heating_option': 'from_file', # INCLUDE THIS FOR HEATING
+        'local_heating_file': inputfile1 # INCLUDE THIS FOR HEATING
     },
 
     'diag_manager_nml': {
@@ -136,5 +137,6 @@ exp.set_resolution(*RESOLUTION)
 #Let's do a run!
 if __name__ == '__main__':
     exp.run(1, num_cores=NCORES, use_restart=False)
-    for i in range(2, 85): #~7y worth (inc. 2y of spin-up)
+    #exp.run(1, num_cores=NCORES, restart_file='/disco/share/rm811/isca_data/'+exp_name+'/restarts/res0106.tar.gz', use_restart=True)
+    for i in range(2, 505): #~42y worth (inc. 2y of spin-up)
         exp.run(i, num_cores=NCORES)  # use the restart i-1 by default
