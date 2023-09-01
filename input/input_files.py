8988e3833e94f4fdf01b2ec0194f5b6488c1aa25
Regan Mudhar <rm811@emps-gv1.ex.ac.uk>
2023-09-01 14:31:39 +0100

    included option to plot upper troposphere equatorial heating following Butler et al. 2010

--- input/input_files.py
+++ input/input_files.py
@@ -100,7 +100,7 @@ def ideal_topo(y_min=25., y_max=65, h_0=6000., m=2., save_output=True):
     
     return filename
 
-def polar_heating(y_wid=20., th_mag=4., p_th = 50., p_top=800., p_ref=800., save_output=True):
+def polar_heating(y_wid=15., th_mag=4., p_th = 50., p_top=600., p_ref=800., save_output=True):
     
     # Parameter sweep
     # 1. Vary p_top - depth of forcing: 0:200:800 (1000 would be no forcing!)
@@ -225,7 +225,7 @@ def combo_heat1(y_wid=15., th_mag=4., p_top = 800., p_th = 50., p_ref=800., q_0=
 
     return filename
 
-def offpole_heating(q_0=4., x_cen=75., y_cen=90., x_wid=5., y_wid=30., p_top = 800., p_th = 50., p_ref=800., save_output=True):
+def offpole_heating(q_0=4., x_cen=45., y_cen=180., x_wid=5., y_wid=15., p_top = 800., p_th = 50., p_ref=800., save_output=True):
     
     # Parameters
     # 1. q_0 - magnitude of forcing in K/day
@@ -261,7 +261,7 @@ def offpole_heating(q_0=4., x_cen=75., y_cen=90., x_wid=5., y_wid=30., p_top = 8
 
     return filename
 
-def combo_heat2(q_0=4., x_cen=75., y_cen=90., x_wid=5., y_wid=30., p_top = 800., p_th = 50., p_ref=800., q_0_2=6., m=2., y_cen_2=45., p_0=800., p_t=200., save_output=True):
+def combo_heat2(q_0=4., x_cen=45., y_cen=180., x_wid=5., y_wid=15., p_top = 800., p_th = 50., p_ref=800., q_0_2=6., m=2., y_cen_2=45., p_0=800., p_t=200., save_output=True):
     
     ds, template = set_up("3d")
     coord_list = ["pfull", "lat", "lon"]
@@ -315,6 +315,99 @@ def combo_heat2(q_0=4., x_cen=75., y_cen=90., x_wid=5., y_wid=30., p_top = 800.,
 
     return filename
 
+def upper_trop_heating(q_0=4., x_cen=0., p_cen=300., x_wid=27., p_wid=125., save_output=True):
+    
+    # Parameters
+    # 1. q_0 - magnitude of forcing in K/day
+    # 2. x/p_cen - center of heating in latitude/pressure
+    # 3. x/p_wid - width of heating in latitude/pressure
+    
+    ds, template = set_up("ozone")
+    coord_list = ["pfull", "lat", "lon"]
+    coords = ds.coords
+    
+    # Vary with latitude and pressure following Butler et al. 2010
+    heat_lat = np.exp(-(ds.lat - x_cen)**2/(2 * x_wid**2.)) * template
+    heat_p = np.exp(-(ds.pfull - p_cen)**2/(2 * p_wid**2.)) * template
+    UT_heat = q_0 * heat_lat * heat_p /86400.
+    
+    if save_output:
+        # NB filename should be 32 characters or less
+        filename = 'q' + str(int(q_0)) + 'x' + str(int(x_cen)) + 'p' + str(int(p_cen)) + 'w' + str(int(x_wid)) + 'h' + str(int(p_wid))
+        print(len(filename))
+        save_heat(UT_heat, 'equator_heating/', filename, coord_list, coords)
+    
+    save_heat(UT_heat, 'equator_heating/', filename, coord_list, coords)   
+
+    return filename
+
+def combo_heat3(y_wid=15., th_mag=4., p_th = 50., p_top=600., p_ref=800., \
+                q_0_1=4., x_cen=0., p_cen=300., x_wid=27., p_wid=125.,  \
+                q_0_2=6., m=2., y_cen=45., p_0=800., p_t=200., save_output=True):
+    
+    ds, template = set_up("3d")
+    coord_list = ["pfull", "lat", "lon"]
+    coords = ds.coords
+
+    ds, template = set_up("3d")
+    coord_list = ["pfull", "lat", "lon"]
+    coords = ds.coords
+
+    # Start with polar heating
+    # Parameters
+    # 1. p_top - depth of forcing: 0:200:800 (1000 would be no forcing!)
+    # 2. th_mag - magnitude of forcing in K/s when centred on 800hPa  (0.5,1.,1.5,2.) Could try negative values too
+    # 3. y_wid - decay of forcing away from pole (10., 15., 20.)
+    # 4. p_th - sets vertical gradient of forcing at cap (25.,50.,75.) Sensitivity check that steepness of transition doesn't cause unexpected behaviour
+    
+    # Vary with latitude in similar way to Orlanski and Solman 2010
+    #heat_lat = np.exp(-((data.lat - 90.)/y_wid)**2.) + np.exp(-((data.lat + 90.)/y_wid)**2.) * template
+    heat_lat = np.exp(-((ds.lat - 90.)/y_wid)**2.) * template # for north pole only
+        
+    # fix so that the function has magnitude 1 when p_top = p_ref, and otherwise scales to give constant net energy input
+    # p_ref can be varied to alter the total input, which I think will be (1000-p_ref) * cp/g * th_mag
+    
+    if p_top==0.:  # If the heating is going right to the model top then make heating uniform in height, scaled to fit p_ref level
+        polar_heat = th_mag * (1000. - p_ref)/1000. * heat_lat /86400.
+    else:
+        polar_heat = 0.5 * th_mag * (1000. - p_ref)/(1000. - p_top) * heat_lat * (1. + np.tanh((ds.pfull - p_top)/p_th)) /86400.
+    
+    # NB filename should be 32 characters or less
+    filename = 'polar_UT_midlat_test'
+    print(len(filename))
+    save_heat(polar_heat, 'asymmetry/', filename, coord_list, coords)
+
+    # Now do upper tropospheric equator heating
+    # Parameters
+    # 1. q_0 - magnitude of forcing in K/day
+    # 2. x/p_cen - center of heating in latitude/pressure
+    # 3. x/p_wid - width of heating in latitude/pressure
+    
+    # Vary with latitude and pressure following Butler et al. 2010
+    heat_lat = np.exp(-(ds.lat - x_cen)**2/(2 * x_wid**2.)) * template
+    heat_p = np.exp(-(ds.pfull - p_cen)**2/(2 * p_wid**2.)) * template
+    UT_heat = q_0_1 * heat_lat * heat_p /86400.
+
+    # Now do zonally asymmetric heat perturbation
+    # Parameters
+    # 1. q_0 - magnitude of forcing in K/day
+    # 2. m - longitudinal wave number (1 or 2)
+    # 3. y_cen - center of heating in latitude
+    # 4. p_0 and p_t - lower and upper bounds in pressure (hPa) respectively
+    y_w = 0.175*360/(2*np.pi)
+
+    heat_lat = np.exp(-0.5 * ((ds.lat - y_cen)/y_w)**2.) * template
+    heat_lon = np.cos(m * np.deg2rad(ds.lon)) * template
+    heat_p = np.sin(np.pi * np.log(ds.pfull/p_0)/np.log(p_t/p_0)) * template
+
+    heat = q_0_2 * heat_lat * heat_lon *  heat_p /86400.  # convert to K/s
+    heat = heat.where(heat['pfull']<p_0, 0)
+    heat = heat.where(heat['pfull']>p_t, 0)
+
+    save_heat(polar_heat + UT_heat + heat, 'asymmetry/', filename, coord_list, coords)   
+
+    return filename
+
 def plot_vertical(folder, filename):
 
     file = '/home/links/rm811/Isca/input/' + folder + '/' + filename + '.nc'
@@ -327,7 +420,10 @@ def plot_vertical(folder, filename):
         h = 8 / 86400
         #h = int(filename.partition("a")[2][0])/86400 * 3
         heat = ds.sel(lon=180, method='nearest').variables[filename]
-    inc = 0.25e-5
+    elif folder == 'equator_heating':
+        h = int(filename.partition("q")[2][0])/86400
+        heat = ds.sel(lon=180, method='nearest').variables[filename]
+    inc = 0.5e-5
 
     lat = ds.coords['lat'].data
     p = ds.coords['pfull'].data 
@@ -339,9 +435,9 @@ def plot_vertical(folder, filename):
     cb.set_label(label=r'Heating (K s$^{-1}$)', size='xx-large')
     cb.ax.tick_params(labelsize='xx-large')
     plt.xlabel(r'Latitude ($\degree$N)', fontsize='xx-large')
-    plt.xticks([10, 30, 50, 70, 90], ['10', '30', '50', '70', '90'])
-    plt.xlim(0, 90)
-    plt.ylim(max(p), 100)
+    #plt.xticks([10, 30, 50, 70, 90], ['10', '30', '50', '70', '90'])
+    plt.xlim(-90, 90)
+    plt.ylim(max(p), 30)
     plt.yscale('log')
     plt.ylabel('Pressure (hPa)', fontsize='xx-large')
     #plt.title(filename, fontsize='x-large')
@@ -411,7 +507,8 @@ def plot_horizontal2(folder, filename):
     return plt.close()
 
 if __name__ == '__main__': 
-    option = input("a) polar heat, b) heat perturb, c) idealised topography, d) combo of a and b?, e) off-pole heat, f) combo of e and b, or g) combo of a and c?")
+    option = input("a) polar heat, b) heat perturb, c) idealised topography, d) combo of a and b?, \
+                   e) off-pole heat, f) combo of e and b, g) combo of a and c, h) upper troposphere heating, or i) polar+UT+midlat?")
 
     H = 8
     p0 = 1000
@@ -443,3 +540,9 @@ if __name__ == '__main__':
     elif option =='g':
         filename = combo_topo()
         plot_horizontal1('asymmetry', filename)
+    elif option =='h':
+        filename = upper_trop_heating()
+        plot_vertical('equator_heating', filename)
+    elif option =='i':
+        filename = combo_heat3()
+        plot_vertical('asymmetry', filename)
